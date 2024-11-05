f9064767cae25b047241e54ace8fe614664fa6eb
Lukas Graff <Lukas.Graff@brightstraining.com>
2024-11-05 16:01:16 +0100

    fixed the json loader, adjusted classes and testing it in program.cs. Also added gitignore files for each folder.

--- LabyrinthGame.ConsoleApp/Program.cs
+++ LabyrinthGame.ConsoleApp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace LabyrinthGame.Core;
+class Program
+{
+    static void Main()
+    {
+        // Ange filvägen till din JSON-fil
+        string filePath = @"E:\OneDrive - Brights\TMP\GroupProjects\LabyrinthGame\scenarios.json"; // Eller den fullständiga vägen till din JSON-fil
+
+        // Ladda scenarier från filen
+        ScenarioCollection scenarios = JsonLoader.LoadScenariosFromFile(filePath);
+
+        if (scenarios != null)
+        {
+            // Skriv ut alla scenarier och deras alternativ
+            foreach (var scenario in scenarios.Scenarios)
+            {
+                Console.WriteLine($"Scenario {scenario.Id}: {scenario.Description}");
+
+                foreach (var option in scenario.Options)
+                {
+                    Console.WriteLine($"  - {option.OptionsText} (Next: {option.NextScenarioId})");
+                }
+            }
+        }
+    }
+}
